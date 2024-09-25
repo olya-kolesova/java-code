fa6cace2a4f459dc07974a1a795008d3c219b785
Olga Kolesova <olka.kolesova@gmail.com>
2024-09-25 15:02:11 +0100

    deleted files

--- local_date_time/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-public class Main {
-    public static void main(String[] args) {
-        DateParser dateParser = new DateParser();
-        dateParser.printDate(LocalDateTime.now());
-
-    }
-}
