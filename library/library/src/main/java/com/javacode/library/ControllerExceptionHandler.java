e55c32d5840fbbfa09fa26889e8e1159b11ed81c
Olga Kolesova <olka.kolesova@gmail.com>
2024-10-06 17:44:29 +0100

    added exception handler for controllers

---
+++ library/library/src/main/java/com/javacode/library/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.javacode.library;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+}
